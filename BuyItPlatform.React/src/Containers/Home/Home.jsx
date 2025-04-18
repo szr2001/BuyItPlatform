70c1715ead90f0d07033566c32d511967e49bc61
Szabo Robert <68609388+szr2001@users.noreply.github.com>
2025-04-18 22:19:08 +0300

    Added prototype search bar

--- BuyItPlatform.React/src/Containers/Home/Home.jsx
+++ BuyItPlatform.React/src/Containers/Home/Home.jsx
@@ -1,41 +1,21 @@
-import './Home.css'
+﻿import './Home.css'
 function Home() {
   return (
         <main>
-            <div class="Holder">
-                <section class="content">
-                    <h2>Welcome, Traveler</h2>
-                    <p>Explore the tales of knights, castles, and the mysteries of medieval times.</p>
-                </section>
-                <section class="content">
-                    <h2>Medieval Lore</h2>
-                    <p>From the rise of kingdoms to the battles of honor, uncover the past through ancient scrolls.</p>
-                </section>
-                <section class="content">
-                    <h2>Medieval Lore</h2>
-                    <p>From the rise of kingdoms to the battles of honor, uncover the past through ancient scrolls.</p>
-                </section>
+          <div className = "holder">
+              <form className="searchbar">
+                  <div className="searchbar-element searchbar-element-obj rounded-left">
+                        <img className = "searchbar-img"/>
+                        <input className="searchbar-text" autoComplete="off" type="text" id="searchItem" placeholder="Searching for Treasure?"/>
+                  </div>
+                  <div className="searchbar-element rounded-right">
+                      <img className="searchbar-img" />
+                      <input className="searchbar-text" autoComplete="off" type="text" id="searchLocation" placeholder="Village?" />
+                  </div>
+                  <button className="searchbar-button" type="submit">Search</button>
+              </form>
           </div>
-          <div class="Holder">
-              <section class="content">
-                  <h2>Welcome, Traveler</h2>
-                  <p>Explore the tales of knights, castles, and the mysteries of medieval times.</p>
-              </section>
-              <section class="content">
-                  <h2>Medieval Lore</h2>
-                  <p>From the rise of kingdoms to the battles of honor, uncover the past through ancient scrolls.</p>
-              </section>
-              <section class="content">
-                  <h2>Medieval Lore</h2>
-                  <p>From the rise of kingdoms to the battles of honor, uncover the past through ancient scrolls.</p>
-              </section>
-          </div>
-          <div class="Holder">
-              <section class="content">
-                  <h2>Medieval Lore</h2>
-                  <p>From the rise of kingdoms to the battles of honor, uncover the past through ancient scrolls.</p>
-              </section>
-          </div>
+
         </main>
   );
 }
